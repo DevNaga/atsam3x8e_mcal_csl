2fadb1a66712592d8f627b06c2d58ebbbebe999b
Dev <devendra.aaru@outlook.com>
2020-07-23 09:55:40 -0700

    add PMC clock enable for some reg

--- src/atsam3x8e_bsp/BSP/PMC/atsam3x8e_pmc.c
+++ src/atsam3x8e_bsp/BSP/PMC/atsam3x8e_pmc.c
@@ -4,3 +4,59 @@
  * 
  * @copyright - All rights reserved Devendra Naga (devendra.aaru@outlook.com) 2020-present
  */
+
+#include "atsam3x8e_pmc.h"
+#include "atsam3x8e_peripherals.h"
+
+void PMC_Enable_UOTGCLK()
+{
+    unsigned int *pmc_uotgclk = (unsigned int *)PMC_SCER;
+
+    *pmc_uotgclk |= 0x00000020;
+}
+
+void PMC_Enable_PCK_0()
+{
+    unsigned int *pmc_pck0 = (unsigned int *)PMC_SCER;
+
+    *pmc_pck0 |= 0x00000100;
+}
+
+void PMC_Enable_PCK_1()
+{
+    unsigned int *pmc_pck1 = (unsigned int *)PMC_SCER;
+
+    *pmc_pck1 |= 0x00000200;
+}
+
+void PMC_Enable_PCK_2()
+{
+    unsigned int *pmc_pck2 = (unsigned int *)PMC_SCER;
+
+    *pmc_pck2 |= 0x00000400;
+}
+
+void PMC_Enable_Periph_CAN_0()
+{
+    unsigned int *pmc_periph_clk = (unsigned int *)PMC_PCER1;
+
+    *pmc_periph_clk |= 0x00000800;
+}
+
+void PMC_Enable_Periph_CAN_1()
+{
+    unsigned int *pmc_periph_clk = (unsigned int *)PMC_PCER1;
+
+    *pmc_periph_clk |= 0x00010000;
+}
+
+void PMC_Enable_Peripheral_clock(enum peripheral_ids id)
+{
+    unsigned int *pmc_periph_clk = 0x0;
+
+    if ((id >= 0) && (id <= 31)) {
+
+    } else {
+        pmc_periph_clk = (unsigned int *)PMC_PCER1;
+    }
+}
--- src/atsam3x8e_bsp/BSP/PMC/atsam3x8e_pmc.h
+++ src/atsam3x8e_bsp/BSP/PMC/atsam3x8e_pmc.h
@@ -7,17 +7,30 @@
 #ifndef __ATSAM3x8E_PMC_H__
 #define __ATSAM3x8E_PMC_H__
 
+// PMC system clock enable register
+#define PMC_SCER 0x400E0600
+
 // PMC system clock status register
 #define PMC_SCSR 0x400E0608
 
 // PMC peripheral clock enable register - write only
 #define PMC_PCER0 0x400E0610
 
+#define PMC_PCER1 0x400E0700
+
 // get USB OTG clock .. 5 th bit
 #define PMC_SCSR_USB_OTG_CLK_GET(__val) (__val = !!((*(unsigned int *)(PMC_SCSR)) >> 5))
 
 // enable USB OTG
 #define PMC_SCSR_USB_OTG_CLK_EN() ((*(unsigned int *)(PMC_SCSR)) |= 0x20)
+
+void PMC_Enable_UOTGCLK();
+
+void PMC_Enable_PCK_0();
+
+void PMC_Enable_PCK_1();
+
+void PMC_Enable_PCK_2();
 
 #endif
 
